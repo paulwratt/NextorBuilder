1040153668302c2d8f474a48a9aa4b07d5db7f72
Francesc Vendrell <xescoo@gmail.com>
2019-01-20 12:29:15 +0100

    Added a few options
    - FDSIK compile only
    - Keep VM running
    - Help + examples
    - Removed sdcc patch (not needed anymore)
    - Updated REDME

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # NextorBuilder
-Build [Nextor](https://github.com/Konamiman/Nextor) (MSX disk operating system) from Linux using Vagrant and VirtualBox
+Build [Nextor](https://github.com/Konamiman/Nextor) (MSX disk operating system) from Linux/OSX using Vagrant and VirtualBox
 
 ## Requirements
 
@@ -8,21 +8,37 @@ Build [Nextor](https://github.com/Konamiman/Nextor) (MSX disk operating system) 
 
 ## How to compile Nextor
 
+From the main directory of this repo run:
 ```console
 $ compile-all.sh
 ```
 
-First execution will take quite a long time as the Windows image will be downloaded
+Check available options with **compile-all --help**:
+```console
+$ compile-all.sh --help
+Use: ./compile-all.sh [-h/--help] | [-f/--fdisk ] | [-n/--nostop]
+	-h/--help: show this help
+	-f/--fdisk: compile FDISK only
+	-n/--nostop: keep VM running
+Examples:
+	./compile-all.sh                    # Compile all and stop VM
+	./compile-all.sh -n                 # Do not stop de VM
+	./compile-all.sh -n -f              # Do not stop de VM and compile FDISK only
+	./compile-all.sh --nostop --fdisk   # Same as previous with long options
+	./compile-all.sh --help             # Show help
+```
+
+Note that first execution might take a long time because the Windows image will be downloaded
 
 ## Why do I need this?
 
 I do not have a Windows machine to compile Nextor and I don't want to install and manage a full GUI
-virtual machine. I'd like to use a Linux terminal to edit the code and try my changes as I normally do.
+virtual machine. I want to use a Linux terminal to edit the code and try my changes as I normally do.
 
 ## How does it work?
 
 I have created a ready made Windows Vagrant image with all the tools needed to compile Nextor.
-There's a script called **compile-all.sh** that does the following: (check the code, it's very simple)
+There's a script called **compile-all.sh** that does the following: (check the [code](https://github.com/xesco/NextorBuilder/blob/master/compile-all.sh) is very simple)
 
 * Checks for requirements
 * Clones Nextor project from github (if not present)
@@ -32,12 +48,15 @@ There's a script called **compile-all.sh** that does the following: (check the c
 
 ## Issues & Comments
 
-* There's an error compiling FDISK which I am trying to solve with Nextor's original developer. Meanwhile I am applying
-my own patch to solve the problem. That's why there's a **sdcc.patch** file.
-* I have added a sample output for a successfull compilation as a reference in **output.log**.
+* I have added a sample output for a successfull compilation as a reference in **output.log**
 * To completely delete the Windows image try with: *vagrant destroy*
+* Please support Nextor's original developer for his amazing work [here](https://www.patreon.com/konamiman)
 
 ## That's it
 
 I know this tool won't be used at all. It's a very small niche of knowledge. But if you happen to encounter this project
 and find it useful, please let me know! Thanks :)
+
+## TODO
+
+* Add suport for other providers: VMWare Fusion and VMWare Workstation
--- compile-all.sh
+++ compile-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FDISK_ONLY=0
+NO_STOP=0
 SOURCE="c:\vagrant\Nextor\source"
 
 # check requirements
@@ -7,47 +9,100 @@ command -v vagrant    >/dev/null 2>&1 || { echo "Please install Vagrant"; exit 1
 command -v virtualbox >/dev/null 2>&1 || { echo "Please install VirtualBox"; exit 1; }
 command -v git        >/dev/null 2>&1 || { echo "Please install git"; exit 1; }
 
+function display_help() {
+    echo "Use: $0 [-h/--help] | [-f/--fdisk ] | [-n/--nostop]"
+    echo "	-h/--help: show this help"
+    echo "	-f/--fdisk: compile FDISK only"
+    echo "	-n/--nostop: keep VM running"
+    echo "Examples:"
+    echo "	$0                    # Compile all and stop VM"
+    echo "	$0 -n                 # Do not stop de VM"
+    echo "	$0 -n -f              # Do not stop de VM and compile FDISK only"
+    echo "	$0 --nostop --fdisk   # Same as previous with long options"
+    echo "	$0 --help             # Show help"
+}
+
+while :
+do
+  case "$1" in
+    -h | --help)
+    display_help
+    exit 0
+    ;;
+    -f | --fdisk)
+    FDISK_ONLY=1
+    shift 1
+    ;;
+    -n | --nostop)
+    NO_STOP=1
+    shift 1
+    ;;
+    -*)
+    echo "Error: Unknown option: $1" >&2
+    exit 1
+    ;;
+    *)  # No more options
+    break
+    ;;
+  esac
+done
+
 if [ ! -d ./Nextor ]; then
     echo "Cloning Nextor..."
     git clone git@github.com:Konamiman/Nextor.git
-    echo "Applying patch..."
-    cd ./Nextor && git apply ../sdcc.patch && cd ..
+fi
+
+vagrant status | grep "running" 2>&1 >/dev/null
+if [ $? -eq 0 ]; then
+    IS_RUNNING=1
+else
+    IS_RUNNING=0
+fi
+
+# start VM (only if it's not running)
+if [ $IS_RUNNING -eq 0 ]; then
+    echo "Starting machine..."
+    vagrant up
+    IS_RUNNING=1
+    echo "done!"
+else
+    echo "skip... (Start VM)"
+fi
+
+if [ $FDISK_ONLY -eq 1 ]; then
+    echo "Compiling FDISK only..."
+    vagrant winrm --shell cmd --command "cd ${SOURCE}\kernel\bank5 && compile.bat"
+    echo "done!"
+else
+    # build nextor kernel
+    echo "Building the kernel..."
+    vagrant winrm --shell cmd --command "cd ${SOURCE}\kernel && compile.bat"
+    echo "done!"
+
+    # build command line tools
+    echo "Building CLI tools..."
+    vagrant winrm --shell cmd --command "cd ${SOURCE}\tools && compile.bat"
+    echo "done!"
+
+    # build nextor.sys
+    echo "Building nextor.sys..."
+    vagrant winrm --shell cmd --command "cd ${SOURCE}\command\msxdos && compile.bat"
+    echo "done!"
+
+    # build command2.com
+    echo "Building command2.com..."
+    vagrant winrm --shell cmd --command "cd ${SOURCE}\command\command && compile.bat"
     echo "done!"
 fi
 
-# start image
-echo "Starting machine..."
-vagrant up
-echo "done!"
-
-# build nextor kernel
-echo "Building the kernel..."
-vagrant winrm --shell cmd --command "cd ${SOURCE}\kernel && compile.bat"
-echo "done!"
-
-# build FDISK <= this is done automatically
-#echo "Building FDISK..."
-#vagrant winrm --shell cmd --command "cd ${SOURCE}\kernel\bank5 && compile.bat"
-#echo "done!"
-
-# build command line tools
-echo "Building CLI tools..."
-vagrant winrm --shell cmd --command "cd ${SOURCE}\tools && compile.bat"
-echo "done!"
-
-# build nextor.sys
-echo "Building nextor.sys..."
-vagrant winrm --shell cmd --command "cd ${SOURCE}\command\msxdos && compile.bat"
-echo "done!"
-
-# build command2.com
-echo "Building command2.com..."
-vagrant winrm --shell cmd --command "cd ${SOURCE}\command\command && compile.bat"
-echo "done!"
-
-# stop machine
-echo "Stopping Windows machine..."
-vagrant suspend
-echo "done!"
+# stop VM (only if it's running or flag not set)
+if [ $IS_RUNNING -eq 1 ] && [ $NO_STOP -eq 0 ]; then
+    echo "Stopping VM..."
+    vagrant suspend
+    IS_RUNNING=0
+    echo "done!"
+else
+    echo "skip...(Stop VM)"
+fi
 
 echo "ALL DONE!"
